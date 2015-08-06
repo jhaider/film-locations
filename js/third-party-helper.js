cf659aeddf7bafc52342dcff14faf089640fc5f7
Jasmine <jasamina@gmail.com>
2015-08-06 00:07:28 -0700

    Moving code. Clean up.

---
+++ js/third-party-helper.js
@@ -0,0 +1,80 @@
+// function initializeScrubber(scrubber, display) {
+//   document.body.appendChild(scrubber.elt);
+//   scrubber.min(1);
+//   scrubber.max(years.length);
+//   scrubber.step(1);
+//   scrubber.value(years.length - 1);
+//   scrubber.orientation('horizontal'); // 'horizontal'
+//   // scrubber.style["width"] = "400px";
+//   document.body.appendChild(display);
+//   display.id = "display";
+// }
+
+function mouseovered(d) {
+  node
+      .each(function(n) { n.target = n.source = false; });
+
+  link
+      .classed("link--target", function(l) { if (l.target === d) return l.source.source = true; })
+      .classed("link--source", function(l) { if (l.source === d) return l.target.target = true; })
+    .filter(function(l) { return l.target === d || l.source === d; })
+      .each(function() { this.parentNode.appendChild(this); });
+
+  node
+      .classed("node--target", function(n) { return n.target; })
+      .classed("node--source", function(n) { return n.source; });
+}
+
+function mouseouted(d) {
+  link
+      .classed("link--target", false)
+      .classed("link--source", false);
+
+  node
+      .classed("node--target", false)
+      .classed("node--source", false);
+}
+
+function packageHierarchy(filmData) {
+  var map = {};
+  var links = filmData;
+
+  function find(name, data) {
+    var node = map[name], i;
+    if (!node) {
+      node = map[name] = data || {name: name, children: []};
+      if (name.length) {
+        node.parent = find(name.substring(0, i = name.lastIndexOf("..")));
+        node.parent.children.push(node);
+        node.key = name.substring(i + 2);
+      }
+    }
+    return node;
+  }
+
+  links.forEach(function(d) {
+    find(d.name, d);
+  });
+
+  return map[""];
+}
+
+// Return a list of imports for the given array of nodes.
+function packageImports(nodes) {
+  var map = {},
+      imports = [];
+
+  // Compute a map from name to node.
+  nodes.forEach(function(d) {
+    map[d.name] = d;
+  });
+
+  // For each import, construct a link from the source to target node.
+  nodes.forEach(function(d) {
+    if (d.imports) d.imports.forEach(function(i) {
+      imports.push({source: map[d.name], target: map[i]});
+    });
+  });
+
+  return imports;
+}
